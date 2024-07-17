623302709bcd0347be21fdc3a558ad611cfa26d7
muhammad.saad <muhammad.saad@joingsg.com>
2024-07-17 19:12:50 +0200

    Removing unnecessary files

--- .aws-sam/deps/410119d7-8a3a-4a04-9115-6c576c8f440c/aws_utils/boto3_manager.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-LOCAL_ENDPOINT_URL = "http://localhost.localstack.cloud:4566"
-def get_s3_client():
-    return boto3.client('s3', endpoint_url = LOCAL_ENDPOINT_URL)
-
-def get_sqs_client():
-    return boto3.client('sqs', endpoint_url = LOCAL_ENDPOINT_URL)
